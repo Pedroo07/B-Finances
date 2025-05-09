be1a0175112506c07d9827426b1d8cb77b38fccd
Pedroo07 <pedrohenriquemoalive@gmail.com>
2025-05-09 00:20:29 -0300

    implementing user login system

--- app/login/components/Main.tsx
+++ app/login/components/Main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { auth } from '@/lib/firebase'
+import { useRouter } from 'next/navigation'
 
 export const Main = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+  const [
+    SignInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth)
+
+  const handleSignIn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    if (!email || !password) return
+    SignInWithEmailAndPassword(email.trim(), password)
+  }
+
+  useEffect(() => {
+    if (user) {
+      router.push('/')
+    }
+  }, [user, router])
   return (
     <div className='flex items-center justify-center flex-col h-screen gap-4 text-center w-[380px] mx-auto' >
       <div>
@@ -12,12 +37,13 @@ export const Main = () => {
         <h2 className='text-lg font-medium dark:text-white '>Log in to manage your finances with ease.</h2>
       </div>
       <div className='w-full flex flex-col gap-1'>
-        <Input type='email' placeholder='Email' className='h-12' />
-        <Input type='password' placeholder='Password' className='h-12' />
+        <Input type='email' placeholder='Email' className='h-12' onChange={(e) => setEmail(e.target.value)} />
+        <Input type='password' placeholder='Password' className='h-12' onChange={(e) => setPassword(e.target.value)} />
       </div>
       <div className='flex flex-col gap-4'>
-        <Button><Link href='/login'>Login</Link></Button>
-        <p>If you don’t have an account, <Link href='/register' className='underline  text-slate-700 dark:text-slate-300'>register now</Link>!</p>
+        <Button onClick={handleSignIn} disabled={!password || !email}>Login</Button>
+        {error?.message && <p className="text-red-500 text-sm">{error.message}</p>}
+        <p>If you don’t have an account, <Link href='/register' className='underline  text-slate-700 dark:text-slate-400'>Register now</Link>!</p>
       </div>
     </div>
   )
