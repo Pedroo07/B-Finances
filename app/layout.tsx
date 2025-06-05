17f3fb5dd8f8b7d48362e244a510bea5d9956be7
Pedroo07 <pedrohenriquemoalive@gmail.com>
2025-06-04 22:55:34 -0300

    Implement global AuthProvider for session persistence

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,7 +9,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-neutral-100 dark:bg-slate-800">
+        <AuthProvider>
         {children}
+        </AuthProvider>
       </body>
     </html>
   );
